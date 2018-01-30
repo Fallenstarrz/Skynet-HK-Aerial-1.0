424676840452775551ca6b45861ede49f9b58e0b
CHRISTOPHER KOON <CHRKOON@UAT.EDU>
2018-01-30 13:21:52 -0700

    Farther Instructions were given.

--- Skynet-HK-Aerial 1.0/Skynet-HK-Aerial/Skynet-HK-Aerial/Skynet-HK-Aerial.cpp
+++ Skynet-HK-Aerial 1.0/Skynet-HK-Aerial/Skynet-HK-Aerial/Skynet-HK-Aerial.cpp
@@ -11,30 +11,33 @@ using namespace std;
 int main()
 {
 	srand(static_cast<unsigned int>(time(0))); // Seed random number generator
-	const int secretNumber = rand()%100 + 1; // number that the computer is trying to guess
+	int highNum = 64;
+	int lowNum = 1;
+	const int secretNumber = rand()lowNum + highNum; // number that the computer is trying to guess
 	int guess; // This variable stores the guess
 	int tries = 0; // This variable holds the number of attempts required to guess the secretNumber correctly
+	bool targetFound = false;
 
 	cout << "Skynet HK-Aerial" << endl << endl;
 
 	do
 	{
-		cout << "Enter a number between 1 and 100" << endl;
-		cin >> guess;
+		cout << "Enter a number between 1 and 64" << endl;
+		cin >> guess; // Make This random from computer
 		if (guess > secretNumber)
 		{
-			cout << "Too high!" << endl;
+			cout << "Too high!" << endl; // Change guess to exclude numbers above our guess
 			tries++;
 		}
 		else if (guess < secretNumber)
 		{
-			cout << "Too low!" << endl;
+			cout << "Too low!" << endl; // Change guess to exclude numbers below our guess
 			tries++;
 		}
 		else
 		{
 			tries++;
-			cout << "You got it correct, and it only took you "<<  tries << "to get it correctly!" << endl;
+			cout << "You got it correct, and it only took you "<<  tries << "to get it correctly!" << endl; // Some flavor text here for found the enemy.
 			
 		}
 
